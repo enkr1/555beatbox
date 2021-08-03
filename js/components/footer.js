91770e71d0eef97d1d3ee21e7fcebcb9d1cd0dfd
Jing Hui P <enkr99@gmail.com>
2021-08-04 06:28:08 +0800

    :sparkles: Footer & contact form

--- js/components/footer.js
+++ js/components/footer.js
@@ -21,37 +21,56 @@ class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
 <footer class="main-footer">
-    <div class="contact-social-media">
-        <ul class="nav-list contact-social-media-ul">
-            <li>
-                <a target="_blank" href="https://www.facebook.com/555-Beatbox-Initiative-104202838540152" class="icon-link">
-                    <i class="fab fa-facebook-square"></i></a>
-            </li>
+    <div class="footer-nav-container">
+        <div class="">
+            <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Rerum sint doloribus distinctio vitae numquam laboriosam sapiente dolore, at suscipit, 
+            quae nobis? Ducimus consectetur magnam inventore voluptas vitae distinctio cupiditate 
+            repellendus.
+            </p>
+        </div>
+        <div class="">
+            <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Rerum sint doloribus distinctio vitae numquam laboriosam sapiente dolore, at suscipit, 
+            quae nobis? Ducimus consectetur magnam inventore voluptas vitae distinctio cupiditate 
+            repellendus.
+            </p>
+        </div>
+        <div class="contact-social-media">
+        <p>find us on</p>
+            <ul class="nav-list contact-social-media-ul">
+                <li>
+                    <a target="_blank" href="https://www.facebook.com/555-Beatbox-Initiative-104202838540152" class="icon-link">
+                        <i class="fab fa-facebook-square"></i></a>
+                </li>
 
-            <li>
-                <a target="_blank" href="https://www.instagram.com/555beatboxsg/" class="icon-link">
-                    <i class="fab fa-instagram"></i></a>
-            </li>
+                <li>
+                    <a target="_blank" href="https://www.instagram.com/555beatboxsg/" class="icon-link">
+                        <i class="fab fa-instagram"></i></a>
+                </li>
 
-            <li>
-                <a target="_blank" href="https://www.youtube.com/channel/UC6jcQdnBe8A6nhaubvY6xNw"
-                    class="icon-link">
-                    <i class="fab fa-youtube-square"></i></a>
-            </li>
+                <li>
+                    <a target="_blank" href="https://www.youtube.com/channel/UC6jcQdnBe8A6nhaubvY6xNw"
+                        class="icon-link">
+                        <i class="fab fa-youtube-square"></i></a>
+                </li>
 
-            <li>
-                <a href="mailto:555beatboxsg@gmail.com" class="icon-link">
-                    <i class="far fa-envelope"></i></a>
-            </li>
-        </ul>
+                <li>
+                    <a href="mailto:555beatboxsg@gmail.com" class="icon-link">
+                        <i class="far fa-envelope"></i></a>
+                </li>
+            </ul>
+        </div>
     </div>
 
     <div class="up" id="up" onclick="topFunction()">
         <i class="fas fa-chevron-up"></i>
     </div>
 
-    <p>&copy; 2021 - 555 Beatbox Initiative. All rights reserved.</p>
-    <p>Developed and designed by ENKR</p>
+    <p>&copy; 2021 ENKR. All rights reserved.</p>
+    <p>Developed and designed by <a target="_blank" href="https://enkr1.github.io/">ENKR</a></p>
 
 </footer>
         `;
